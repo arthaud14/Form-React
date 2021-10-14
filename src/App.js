35b25a35cdf3602fb5742871bca2462ebb1ad4f2
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-14 11:41:58 +0200

    Form React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(name && email && message) {
+
+   
+    sendFeedback("template_j169p89", 
+    {
+      name,
+      company,
+      phone,
+      email,
+      message,
+    });
+  } else {
+    console.log('error');
+  }
+  };
+
+  const sendFeedback = (templateId, variables) => {
+    window.emailjs
+      .send("gmail", templateId, variables)
+      .then((res) => {
+        console.log("success !");
+        setName("");
+        setCompany("");
+        setPhone("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch(
+        (err) =>
+          (document.querySelector(".form-message").innerHTML =
+            "Une erreur s'est produite, veuillez réessayer.")
+      );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="contact-form">
+      <h2>Contactez-nous</h2>
+      <div className="form-content">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="nom *"
+          value={name}
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          id="company"
+          name="company"
+          onChange={(e) => setCompany(e.target.value)}
+          placeholder="société"
+          value={company}
+        />
+        <input
+          type="text"
+          id="phone"
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="téléphone"
+          value={phone}
+        />
+        <div className="email-content">
+          <label id="not-mail">Email non valide</label>
+          <input
+            type="mail"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email *"
+            value={email}
+            autoComplete="off"
+          />
+        </div>
+        <textarea
+          id="message"
+          name="message"
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="message *"
+          value={message}
+        />
+      </div>
+      <input
+        className="button"
+        type="button"
+        value="Envoyer"
+        onClick={handleSubmit}
+      />
+      <div className="form-message"></div>
+    </form>
   );
-}
+};
 
 export default App;
